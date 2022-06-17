afd0076bc485c844ab6b6022ae9c285226a03b4a
Ryan Devenney <rwdevenney@gmail.com>
2022-06-17 11:55:51 -0400

    ecs

---
+++ src/systems/mod.rs
@@ -0,0 +1,15 @@
+mod player_input;
+mod map_render;
+mod entity_render;
+
+use crate::prelude::*;
+
+use self::{player_input::player_input, entity_render::entity_render};
+
+pub fn build_scheduler() -> Schedule {
+    Schedule::builder()
+        .add_system(player_input::player_input_system())
+        .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
+        .build()
+}
