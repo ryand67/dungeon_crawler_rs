47819a04540ed1861f00afad5429313915ecbd57
Ryan Devenney <rwdevenney@gmail.com>
2022-06-18 16:18:50 -0400

    enemies and basic collision

---
+++ README.md
@@ -0,0 +1,2 @@
+## Dungeon Crawl rs
+Following along with the Hands-On Rust book by Herbet Wolverson. 
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,6 +1,7 @@
 mod player_input;
 mod map_render;
 mod entity_render;
+mod collisions;
 
 use crate::prelude::*;
 
@@ -9,6 +10,7 @@ use self::{player_input::player_input, entity_render::entity_render};
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(collisions::collisions_system())
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .build()
