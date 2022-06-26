d3b07bdc82fb8b592b17cde227703126e25f2d14
Ryan Devenney <rwdevenney@gmail.com>
2022-06-26 16:42:46 -0400

    Turn based

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -1,15 +1,17 @@
-use crate::prelude::*;
+use crate::{prelude::*, turn_state};
 
 #[system]
 #[write_component(Point)]
 #[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld,
-    #[resource] map: &Map,
+    commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera,
     #[resource] turn_state: &mut TurnState
 ) {
+    let mut players = <(Entity, &Point)>::query()
+        .filter(component::<Player>());
+
     if let Some(key) = key {
         let delta = match key {
             VirtualKeyCode::Left | VirtualKeyCode::A => Point::new(-1, 0),
@@ -19,17 +21,11 @@ pub fn player_input(
             _ => Point::new(0, 0)
         };
 
-        if delta.x != 0 || delta.y != 0 {
-            let mut players = <&mut Point>::query()
-                .filter(component::<Player>());
-            players.iter_mut(ecs).for_each(|pos| {
-                let destination = *pos + delta;
-                if map.can_enter_tile(destination) {
-                    *pos = destination;
-                    camera.on_player_move(destination);
-                    *turn_state = TurnState::PlayerTurn;
-                }
-            })
-        }
+        players.iter(ecs).for_each(| (entity, pos) | {
+            let destination = *pos + delta;
+            commands
+                .push(((), WantsToMove { entity: *entity, destination }));
+        });
+        *turn_state = TurnState::PlayerTurn;
     }
 }
